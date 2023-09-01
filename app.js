22fcd993374e54e2fb29706dd0062d5220f3ce60
chrille0234 <madsen.chr@hotmail.com>
2023-09-01 09:35:27 +0200

    add newsletter box, js and css

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// skriv din JavaScript her...
+const signUpToNewsletter = document.querySelector(".signUpToNewsletter");
+
+document.body.addEventListener("mousemove", function(event){
+    if (event.clientY < 50) signUpToNewsletter.style.display = "block"
+
+})
