87138b15e4a4d12ced83d853eb836cf1fe3b0443
chrille0234 <madsen.chr@hotmail.com>
2023-09-01 13:47:39 +0200

    add stuff

--- colorTheme.js
+++ colorTheme.js
@@ -1,3 +1,4 @@
+"strict mode"
 let colorTheme = [
     {
         name: 'firstTheme',
@@ -21,7 +22,7 @@ let colorTheme = [
         primary: '#FFEB3B',
         secondary: '#CDDC39',
         accent: '#FFC107',
-    },
+    }
 ]
 const rootElement = document.documentElement;
 
@@ -29,10 +30,13 @@ rootElement.style.setProperty('--color-main', colorTheme[0].primary);
 rootElement.style.setProperty('--color-background', colorTheme[0].secondary);
 rootElement.style.setProperty('--color-accent', colorTheme[0].accent);
 
-colorTheme.forEach((theme, index) => {
-    document.getElementById(colorTheme[index].name).addEventListener('input', () => {
-        rootElement.style.setProperty('----color-main', theme.primary);
-        rootElement.style.setProperty('--color-background', theme.secondary);
-        rootElement.style.setProperty('--color-accent', theme.accent);
-    })
-})
+for(let i = 0; i < colorTheme.length; i++){
+    console.log(colorTheme[i]);
+    document.getElementById(colorTheme[i].name).addEventListener('click', function(){
+        rootElement.style.setProperty('--color-main', colorTheme[i].primary);
+        rootElement.style.setProperty('--color-background', colorTheme[i].secondary);
+        rootElement.style.setProperty('--color-accent', colorTheme[i].accent);
+        })
+
+}
+
