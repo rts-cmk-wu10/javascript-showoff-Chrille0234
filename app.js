412a750e9bf90a05385a6f0b6b26d9ffbda8b0db
chrille0234 <madsen.chr@hotmail.com>
2023-09-01 10:23:55 +0200

    add dont ask again button

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const signUpToNewsletter = document.querySelector(".signUpToNewsletter");
+const close = document.querySelector(".signUpToNewsletter__close");
+const emailField = signUpToNewsletter.querySelector("input[type='email']"); 
+const dontaskagain = signUpToNewsletter.querySelector(".dont-ask-again");
+
+let hasChanged = localStorage.getItem("dontaskagain") === "true" ? true : false;
 
 document.body.addEventListener("mousemove", function(event){
-    if (event.clientY < 50) signUpToNewsletter.style.display = "block"
+    if (event.clientY < 50 && !hasChanged) {
+        signUpToNewsletter.style.display = "block"
+        hasChanged = true;
+    }
+})
+close.addEventListener("click", function(){
+    signUpToNewsletter.style.display = "none"
+    emailField.value = "";
+})
 
+dontaskagain.addEventListener("click", function(){
+    signUpToNewsletter.style.display = "none"
+    emailField.value = "";
+    localStorage.setItem("dontaskagain", "true");
 })
